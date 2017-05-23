c3e0927bb63218f42c0fc2bec7cd09e128e35328
lilu <chujunhe1234@163.com>
2017-05-23 18:40:17 +0800

    获取电池信息

--- DeviceInfo/ViewController.m
+++ DeviceInfo/ViewController.m
@@ -8,22 +8,80 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation ViewController
 
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setUpViews];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - functions
+- (void)setUpViews {
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+    self.tableView = tableView;
 }
 
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [tableView reloadData];
+}
 
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *ID = @"UITableViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:ID];
+    }
+    switch (indexPath.row) {
+        case 0:{
+            cell.textLabel.text = @"电池电量";
+            //电池相关
+            [UIDevice currentDevice].batteryMonitoringEnabled = YES;
+            CGFloat batteryLevel = [[UIDevice currentDevice] batteryLevel];
+            NSLog(@"batteryLevel:%f", batteryLevel);
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"batteryLevel:%f", batteryLevel];
+            break;
+        }
+        case 1:{
+            cell.textLabel.text = @"电池状态";
+            /*typedef NS_ENUM(NSInteger, UIDeviceBatteryState) {
+                UIDeviceBatteryStateUnknown,
+                UIDeviceBatteryStateUnplugged,   // on battery, discharging
+                UIDeviceBatteryStateCharging,    // plugged in, less than 100%
+                UIDeviceBatteryStateFull,        // plugged in, at 100%
+            }*/
+            //电池相关
+            [UIDevice currentDevice].batteryMonitoringEnabled = YES;
+            UIDeviceBatteryState batteryState = [[UIDevice currentDevice] batteryState];
+            NSLog(@"batteryState:%ld", (long)batteryState);
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"batteryState:%ld", (long)batteryState];
+            break;
+        }
+        case 2:{
+            
+            break;
+        }
+            
+        default:
+            cell.textLabel.text = @"Device";
+            cell.detailTextLabel.text = @"Info";
+            break;
+    }
+    return cell;
+}
 @end
